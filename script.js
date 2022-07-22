0f9a0a77bf9b5ea87ca02b17ddf2599fdb1f9787
solszym <solszym@gmail.com>
2022-07-23 00:41:56 +0200

    Added exception handling

--- script.js
+++ script.js
@@ -3,22 +3,37 @@ function getComputerChoice() {
     return arr[Math.floor(Math.random() * arr.length)]
 }
 
-function singleRound(playerChoice, computerChoice = getComputerChoice()) {
-    console.log(`computer: ${computerChoice}`)
-    playerChoice = playerChoice.toLowerCase();
-
-    let rules = {
+function singleRound() {
+    let beats = {
         "rock": "scissors",
         "scissors": "paper",
         "paper": "rock"
     }
 
+    let computerChoice = getComputerChoice();
+    let playerChoice = prompt("Rock, paper or scissors?");
+    playerChoice = playerChoice.toLowerCase();
+
+    while (!playerChoice.includes("rock") && !playerChoice.includes("paper") && !playerChoice.includes("scissors")) {
+        console.log("Invalid input!");
+        playerChoice = prompt("Rock, paper or scissors?");
+        playerChoice = playerChoice.toLowerCase();
+    }
+
     if (computerChoice == playerChoice) {
         return "It's a tie!"
-    } else if (rules[playerChoice] == computerChoice) {
+    } else if (beats[playerChoice] == computerChoice) {
         return `You won! ${playerChoice} beats ${computerChoice}.`
     } else {
         return `You lost! ${computerChoice} beats ${playerChoice}.`
     }
 }
 
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(`Round ${i}:`)
+        console.log(singleRound());
+    }
+}
+
+console.log(game())
