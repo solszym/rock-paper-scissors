0d3146b5cda29e5db2d9f8bfedd1179aa5baa165
solszym <solszym@gmail.com>
2022-07-22 23:59:10 +0200

    Created getComputerChoice

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rock-paper-scissors
+Simple console-based (for now) rock, paper, scissors game with a computer.
---
+++ script.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    let arr = ["Rock", "Paper", "Scissors"]
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+console.log(getComputerChoice())
