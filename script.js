8bc8a5a6a292efb485ed0c654662469e7895addf
solszym <solszym@gmail.com>
2022-07-23 00:12:54 +0200

    First working playGame

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function getComputerChoice() {
-    let arr = ["Rock", "Paper", "Scissors"]
+    let arr = ["rock", "paper", "scissors"]
     return arr[Math.floor(Math.random() * arr.length)]
 }
 
-console.log(getComputerChoice())
+function playGame(playerChoice, computerChoice = getComputerChoice()) {
+    console.log(`computer: ${computerChoice}`)
+    playerChoice = playerChoice.toLowerCase();
+
+    let rules = {
+        "rock": "scissors",
+        "scissors": "paper",
+        "paper": "rock"
+    }
+
+    if (computerChoice == playerChoice) {
+        return "It's a tie!"
+    } else if (rules[playerChoice] == computerChoice) {
+        return "You won!"
+    } else {
+        return "You lost!"
+    }
+
+}
+
+console.log(playGame("rock"));
