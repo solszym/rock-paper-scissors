759797762fbf5ec60f672bcfb0f574c4ea135006
solszym <solszym@gmail.com>
2022-08-10 21:18:46 +0200

    first css and dom manipulation

--- script.js
+++ script.js
@@ -28,29 +28,31 @@ function getComputerChoice() {
 //         return `You lost! ${computerChoice} beats ${playerChoice}.`
 //     }
 // }
+
+
+// function playRound(button) {
+
+//     let beats = {
+//         "rock": "scissors",
+//         "scissors": "paper",
+//         "paper": "rock"
+//     }
+
+//     let computerChoice = getComputerChoice();
+//     let playerChoice = button.getAttribute('id')
+//     console.log(`your choice: ${playerChoice}`)
+
+//     if (computerChoice == playerChoice) {
+//         console.log("It's a tie!")
+//     } else if (beats[playerChoice] == computerChoice) {
+//         console.log(`You won! ${playerChoice} beats ${computerChoice}.`)
+//     } else {
+//         console.log(`You lost! ${computerChoice} beats ${playerChoice}.`)
+//     }
+// }
+
 const buttons = document.querySelectorAll("button")
-
-function playRound(button) {
-
-    let beats = {
-        "rock": "scissors",
-        "scissors": "paper",
-        "paper": "rock"
-    }
-
-    let computerChoice = getComputerChoice();
-    let playerChoice = button.getAttribute('id')
-    console.log(`your choice: ${playerChoice}`)
-
-    if (computerChoice == playerChoice) {
-        console.log("It's a tie!")
-    } else if (beats[playerChoice] == computerChoice) {
-        console.log(`You won! ${playerChoice} beats ${computerChoice}.`)
-    } else {
-        console.log(`You lost! ${computerChoice} beats ${playerChoice}.`)
-    }
-}
-
+const results = document.querySelector('#results')
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -65,10 +67,13 @@ buttons.forEach(button => {
         console.log(`your choice: ${playerChoice}`)
 
         if (computerChoice == playerChoice) {
+            results.textContent = "It's a tie!"
             console.log("It's a tie!")
         } else if (beats[playerChoice] == computerChoice) {
+            results.textContent = `You won! ${playerChoice} beats ${computerChoice}.`;
             console.log(`You won! ${playerChoice} beats ${computerChoice}.`)
         } else {
+            results.textContent = `You lost! ${computerChoice} beats ${playerChoice}.`;
             console.log(`You lost! ${computerChoice} beats ${playerChoice}.`)
         }
     })
