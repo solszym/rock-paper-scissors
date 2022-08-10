003523285f36e8e4d89acb5f3843a4145ef8e435
solszym <solszym@gmail.com>
2022-08-10 20:32:20 +0200

    first working eventlistener for all buttons

--- script.js
+++ script.js
@@ -3,7 +3,35 @@ function getComputerChoice() {
     return arr[Math.floor(Math.random() * arr.length)]
 }
 
-function singleRound() {
+// function playRoundConsole() {
+//     let beats = {
+//         "rock": "scissors",
+//         "scissors": "paper",
+//         "paper": "rock"
+//     }
+
+//     let computerChoice = getComputerChoice();
+//     let playerChoice = prompt("Rock, paper or scissors?");
+//     playerChoice = playerChoice.toLowerCase();
+
+//     while (!playerChoice.includes("rock") && !playerChoice.includes("paper") && !playerChoice.includes("scissors")) {
+//         console.log("Invalid input!");
+//         playerChoice = prompt("Rock, paper or scissors?");
+//         playerChoice = playerChoice.toLowerCase();
+//     }
+
+//     if (computerChoice == playerChoice) {
+//         return "It's a tie!"
+//     } else if (beats[playerChoice] == computerChoice) {
+//         return `You won! ${playerChoice} beats ${computerChoice}.`
+//     } else {
+//         return `You lost! ${computerChoice} beats ${playerChoice}.`
+//     }
+// }
+const buttons = document.querySelectorAll("button")
+
+function playRound(button) {
+
     let beats = {
         "rock": "scissors",
         "scissors": "paper",
@@ -11,29 +39,73 @@ function singleRound() {
     }
 
     let computerChoice = getComputerChoice();
-    let playerChoice = prompt("Rock, paper or scissors?");
-    playerChoice = playerChoice.toLowerCase();
-
-    while (!playerChoice.includes("rock") && !playerChoice.includes("paper") && !playerChoice.includes("scissors")) {
-        console.log("Invalid input!");
-        playerChoice = prompt("Rock, paper or scissors?");
-        playerChoice = playerChoice.toLowerCase();
-    }
+    let playerChoice = button.getAttribute('id')
+    console.log(`your choice: ${playerChoice}`)
 
     if (computerChoice == playerChoice) {
-        return "It's a tie!"
+        console.log("It's a tie!")
     } else if (beats[playerChoice] == computerChoice) {
-        return `You won! ${playerChoice} beats ${computerChoice}.`
+        console.log(`You won! ${playerChoice} beats ${computerChoice}.`)
     } else {
-        return `You lost! ${computerChoice} beats ${playerChoice}.`
+        console.log(`You lost! ${computerChoice} beats ${playerChoice}.`)
     }
 }
 
-function game() {
-    for (let i = 1; i <= 5; i++) {
-        console.log(`Round ${i}:`)
-        console.log(singleRound());
-    }
-}
 
-console.log(game())
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        let beats = {
+            "rock": "scissors",
+            "scissors": "paper",
+            "paper": "rock"
+        }
+
+        let computerChoice = getComputerChoice();
+        let playerChoice = button.getAttribute('id')
+        console.log(`your choice: ${playerChoice}`)
+
+        if (computerChoice == playerChoice) {
+            console.log("It's a tie!")
+        } else if (beats[playerChoice] == computerChoice) {
+            console.log(`You won! ${playerChoice} beats ${computerChoice}.`)
+        } else {
+            console.log(`You lost! ${computerChoice} beats ${playerChoice}.`)
+        }
+    })
+})
+
+// const rock = document.querySelector("#rock")
+// rock.addEventListener('click', () => {
+//     let beats = {
+//         "rock": "scissors",
+//         "scissors": "paper",
+//         "paper": "rock"
+//     }
+
+//     let computerChoice = getComputerChoice();
+//     let playerChoice = "rock"
+
+//     if (computerChoice == playerChoice) {
+//         console.log("It's a tie!")
+//     } else if (beats[playerChoice] == computerChoice) {
+//         console.log(`You won! ${playerChoice} beats ${computerChoice}.`)
+//     } else {
+//         console.log(`You lost! ${computerChoice} beats ${playerChoice}.`)
+//     }
+// });
+
+// const paper = document.querySelector("#paper")
+
+
+// const scissors = document.querySelector("#scissors")
+
+
+
+// function game() {
+//     for (let i = 1; i <= 5; i++) {
+//         console.log(`Round ${i}:`)
+//         console.log(playRound());
+//     }
+// }
+
+// console.log(game())
